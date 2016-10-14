20d69abc0234f5754d62470a9862707e37b50fb1
ericklopez <emlv_pollo@hotmail.com>
2016-10-14 03:19:09 -0500

    Se agregaron las clases Fuego, Agua, Hierba y Electrico

---
+++ JuegoBatallas/src/Electrico.java
@@ -0,0 +1,35 @@
+/**
+ * Clase Electrico que contiene metodo abstracto de ataque y otro que calcula el multiplicador elemental
+ * @author erick
+ */
+public abstract class Electrico extends Monstruo{
+    /**
+     * Metodo que calcula el daño dependiendo el elemento del monstruo al que se ataque
+     * @param elemento
+     * @return 
+     */
+    @Override
+        public float multiplicadorElemental(Monstruo elemento){
+            if (elemento instanceof Agua)
+                return (float) 2.0;
+            else if(elemento instanceof Hierba || elemento instanceof Fuego)
+                return (float) 0.5;
+            else if(elemento instanceof Electrico)
+                return (float) 1.0;
+        }
+    
+    /**
+     * Metodo que calcula el daño a los monstruos enemigos
+     * @param enemigo 
+     */
+    @Override
+        public void ataque1(Monstruo enemigo){
+            float daño;
+            
+            if ((daño = (this.ataque - enemigo.defensa)*multiplicadorElemental(enemigo)) <= 0)
+			daño = 0;
+
+		enemigo.hp -= daño;
+		System.out.println("¡"+enemigo.apodo+" pierde "+daño+" puntos de vida por el ataque de "+this.apodo+"!");
+        }
+}
---
+++ JuegoBatallas/src/Fuego.java
@@ -0,0 +1,36 @@
+/**
+ * Clase Fuego que contiene metodo abstracto de ataque y otro que calcula el multiplicador elemental
+ * @author erick
+ */
+public abstract class Fuego extends Monstruo{
+    
+    /**
+     * Metodo que calcula el daño dependiendo el elemento del monstruo al que se ataque
+     * @param elemento
+     * @return 
+     */
+    @Override
+    public float multiplicadorElemental(Monstruo elemento){
+        if(elemento instanceof Hierba)
+            return (float) 2.0;
+        else if(elemento instanceof Fuego || elemento instanceof Agua)
+            return (float) 0.5;
+        else if(elemento instanceof Electrico)
+            return (float) 1.0;    
+    }
+    
+    /**
+     * Metodo que calcula el daño a los monstruos enemigos
+     * @param enemigo 
+     */
+    @Override
+        public void ataque1(Monstruo enemigo){
+            float daño;
+            
+            if ((daño = (this.ataque - enemigo.defensa)*multiplicadorElemental(enemigo)) <= 0)
+			daño = 0;
+
+		enemigo.hp -= daño;
+		System.out.println("¡"+enemigo.apodo+" pierde "+daño+" puntos de vida por el ataque de "+this.apodo+"!");
+        }
+}
