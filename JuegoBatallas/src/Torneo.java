c698add58b561ee7578b889d6adee5f2ebe2a0fa
mquinteror <martinsilloelfilosofo@gmail.com>
2016-10-15 05:39:27 -0500

    Se agregaron mas cambios a la clase Torneo

--- JuegoBatallas/src/Torneo.java
+++ JuegoBatallas/src/Torneo.java
@@ -19,12 +19,13 @@ public class Torneo {
        nombreContrincante = sc.nextLine();
        c1.nombre = nombreContrincante;
        System.out.println( c1.nombre + "Esgoge tus mounstruos");
-       // contrincante 1 escoge sus mounstruos
+       escogerMonstruos( c1 );
        System.out.println("Contrincante2 Escoge tu neombre: ");
+       nombreContrincante = sc.nextLine();
        nombreContrincante = sc.nextLine();
        c2.nombre = nombreContrincante;
        System.out.println( c2.nombre + "Esgoge tus mounstruos");
-       // contrincante 2 escoge sus mounstruos
+       escogerMonstruos( c2 );
    } 
    
    void menuPrincipal(){
@@ -36,9 +37,6 @@ public class Torneo {
            optPrincipal = sc.nextInt();
            
            switch ( optPrincipal ){
-               case 1:
-                   escogerMonstruos();
-                   break;
                case 2:
                    iniciarTorneo();
                    break;
@@ -52,11 +50,65 @@ public class Torneo {
        System.out.println("el torneo se ha iniciado");
    }
    
-   void escogerMonstruos( Contrincante cont ){
+   void escogerMonstruos( Contrincante cont){
+       int opt = 0;
        generarMonstruos();
        for ( int i = 0; i< 6; i++ ){
            System.out.println("Escoge un monstruo de los siguientes");
-           
+           System.out.println("[1] - Blastoise");
+           System.out.println("[2] - Charizard");
+           System.out.println("[3] - Electabuzz");
+           System.out.println("[4] - Flareon");
+           System.out.println("[5] - Growlithe");
+           System.out.println("[6] - Gyrados");
+           System.out.println("[7] - Jolteon");
+           System.out.println("[8] - Pikachu");
+           System.out.println("[9] - Poliwag");
+           System.out.println("[10] - Tagela");
+           System.out.println("[11] - Venuraus");
+           System.out.println("[12] - Victrebel");
+           opt = sc.nextInt();
+           System.out.println("opt = " +  opt);
+           switch( opt ){
+               case 1:
+                   cont.monstruo.add(new Blastoise(null, 100));
+                   break;
+               case 2:
+                  cont.monstruo.add(new Charizard(null, 100));
+                  break;
+               case 3:
+                   cont.monstruo.add(new Electabuzz(null, 100));
+                   break;
+               case 4:
+                  cont.monstruo.add(new Flareon(null, 100));
+                  break;
+               case 5:
+                   cont.monstruo.add(new Growlithe(null, 100));
+                   break;
+               case 6:
+                   cont.monstruo.add(new Gyarados(null, 100));
+                   break;
+               case 7:
+                   cont.monstruo.add(new Jolteon(null, 100));
+                   break;
+               case 8:
+                   cont.monstruo.add(new Pikachu(null, 100));
+                   break;
+               case 9:
+                   cont.monstruo.add(new Poliwag(null, 100));
+                   break;
+               case 10:
+                   cont.monstruo.add(new Tangela(null, 100));
+                   break;
+               case 11:
+                   cont.monstruo.add(new Venusaur(null, 100));
+                   break;
+               case 12:
+                   cont.monstruo.add(new Victreebel(null, 100));
+                   break;
+               default:
+                   break;
+           }
        }
    }
    void generarMonstruos(){
