2b088fc11d416e8455c6f0f2dfbd17db342c0d3d
mquinteror <martinsilloelfilosofo@gmail.com>
2016-10-15 02:55:02 -0500

    Se añadio la clase jugar para que inicie la ejecucion del programa

---
+++ JuegoBatallas/src/Jugar.java
@@ -0,0 +1,11 @@
+
+/**
+ * Clase Jugar, sirve para iniciar el tonero
+ * @author mquinteror, erick
+ */
+public class Jugar {
+    public static void main ( String args[] ){
+        Torneo t1 = new Torneo();
+        t1.menuPrincipal();
+    }
+}
--- JuegoBatallas/src/Torneo.java
+++ JuegoBatallas/src/Torneo.java
@@ -1,14 +1,46 @@
-
+import java.util.Scanner;
 /**
- * Clase Tonero
- * @author erick, martin
+ * Clase Torneo, es donde se desarrolla y controla lo que sucede en la batalla
+ * @author mquinteror, erick
  */
-package JuegoBatallas;
 
-class Torneo{
-    public static void main ( String args[] ){
-	// comencemos las pruebas
-	System.out.println("Hola, torneo esta funcionando");
-	return 0;
-    }
+public class Torneo {
+   Scanner sc = new Scanner (System.in);
+   
+   public Torneo(){
+       
+       System.out.println("Bienvenido al torneo");
+       Contrincante c1 = new Contrincante();
+       Contrincante c2 = new Contrincante();
+   } 
+   
+   void menuPrincipal(){
+       int optPrincipal = 0;
+       do {
+           System.out.println("[1] - Escoger mounstruos");
+           System.out.println("[2] - Iniciar el Torneo");
+           System.out.println("[0] - Salir");
+           optPrincipal = sc.nextInt();
+           
+           switch ( optPrincipal ){
+               case 1:
+                   escogerMounstruos();
+                   break;
+               case 2:
+                   iniciarTorneo();
+                   break;
+               default:
+                   break;
+           }
+       }while(optPrincipal != 0);
+   }
+   
+   void iniciarTorneo(){
+       System.out.println("el torneo se ha iniciado");
+   }
+   
+   void escogerMounstruos(){
+       System.out.println("se van a escoger los mounstruos");
+   }
+   
 }
